fbe85552a3ac669a8c74b8dd467eaa686f762124
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-05-17 17:09:39 +0300

    implements github cardlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
+function CardList(props) {
+  return (
+    <div>
+      {props.profiles.map((item) => (
+        <Card {...item} />
+      ))}
+      {/* <Card {...testData[0]} />
+      <Card {...testData[1]} />
+      <Card {...testData[2]} /> */}
+    </div>
+  );
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src={this.props.avatar_url} alt="profile-photo" />
+        <div className="info">
+          <div className="name">{this.props.name}</div>
+          <div className="company">{this.props.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { inputValue: "" };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const resp = await axios.get(
+      `https://api.github.com/users/${this.state.inputValue}`
+    );
+    this.props.addUserFunctionality(resp.data);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub user"
+          value={this.state.inputValue}
+          onChange={(event) =>
+            this.setState({ inputValue: event.target.value })
+          }
+        />
+        <button type="submit">Add user</button>
+      </form>
+    );
+  }
+}
+
+function App(props) {
+  //getting acces to profiles both to form and cardlist
+  const [profiles, setProfiles] = useState([]);
+
+  const addUser = (user) => {
+    setProfiles([...profiles, user]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{props.title}</h1>
+      <Form addUserFunctionality={addUser} />
+      <CardList profiles={profiles} />
     </div>
   );
 }
